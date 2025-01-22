93f67d43a27a475ef37d4d866d68e6e0e555f30e
SudharsanGopalakrishnan <sudharsan.g108@gmail.com>
2025-01-22 15:34:44 +0530

    Add files via upload

---
+++ Battery.java
@@ -0,0 +1,21 @@
+public class Battery {
+
+public static void main(String ARGS[]){
+System.out.println("Inside first Battery class");
+System.out.println("Triggering next class");
+}	
+
+
+/*String batteryName, Brand;
+	int cost, lifespan;
+	
+	
+	
+	public Battery(String batteryName, String Brand,int cost,int lifespan) {
+		this.batteryName=batteryName;
+		this.Brand=Brand;
+		this.cost=cost;
+		this.lifespan=lifespan;
+	}*/
+
+}
